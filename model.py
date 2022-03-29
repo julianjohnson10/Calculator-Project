1feb041c3050c295f50d6a2c0d1ab2b91afa91fc
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-28 18:36:24 -0700

    updated model

--- model.py
+++ model.py
@@ -1,4 +1,27 @@
 class Model():
     def __init__(self):
-        pass
+        self.value = ''
 
+    def multiply(a,b):
+        return a * b
+
+
+    def calculate(self, name):
+        #Check to see if the list is empty.
+        #Determine if the button is a number or not.
+        is_int = isinstance(name, int)
+
+        #if it IS a number, it will concatenate the current number value to the Entry object
+        match is_int:
+            case True : self.value += str(name)
+            case False : pass
+
+        match name:
+            case "C":
+                self.value = ''
+            case "+":
+                pass
+            case "/":
+                pass
+
+        return self.value
