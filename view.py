ba52e91f5086e8695d69536b16dedac02dbe4468
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-28 18:36:31 -0700

    updated view

--- view.py
+++ view.py
@@ -1,23 +1,29 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import PhotoImage, ttk
 
 class View(tk.Tk):
+    
     PAD = 10
-    button_names = ["C", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="]
+    button_names = ["C", "+/-", "%", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "="]
 
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
 
         self.title("Calculator")
+        
         self.minsize(400,150)
 
         self.value = tk.StringVar()
 
         self._frame_create()
+        
+        self.iconbitmap('calculator_icon.ico')
         self._create_entry()
-        for button in self.button_names:
-            self._create_button(button)
+
+        for button_name in self.button_names:
+
+            self._create_button(button_name)
 
     def main(self):
         self.mainloop()
@@ -30,6 +36,6 @@ class View(tk.Tk):
         entry = ttk.Entry(self.frame, textvariable=self.value)
         entry.pack()
     
-    def _create_button(self, name):
-        button = ttk.Button(self.frame, text=name)
+    def _create_button(self,name):
+        button = ttk.Button(self.frame, text=name, command=lambda button=name: self.controller.onClick(button))
         button.pack()
