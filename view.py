a94e1226d87609ea827957b28d4d2583024cc437
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-29 00:52:20 -0700

    modify grid

--- view.py
+++ view.py
@@ -1,5 +1,6 @@
+from sre_constants import CATEGORY_UNI_LINEBREAK
 import tkinter as tk
-from tkinter import PhotoImage, ttk
+from tkinter import ttk
 
 class View(tk.Tk):
     
@@ -11,31 +12,42 @@ class View(tk.Tk):
         self.controller = controller
 
         self.title("Calculator")
-        
-        self.minsize(400,150)
+        # self.maxsize(300,420)
+        self.minsize(300,420)
 
         self.value = tk.StringVar()
+        self.value2 = tk.StringVar()
 
         self._frame_create()
+
+        #Rows and Cols
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        
         
         self.iconbitmap('calculator_icon.ico')
-        self._create_entry()
+        # self._create_entry()
+        # self._create_label()
 
-        for button_name in self.button_names:
-
-            self._create_button(button_name)
+        for button in self.button_names:
+            self._create_button(name=button, row=1, column=0)
 
     def main(self):
         self.mainloop()
     
     def _frame_create(self):
         self.frame = ttk.Frame(self)
-        self.frame.pack(padx=self.PAD, pady=self.PAD)
+        self.frame.grid(padx=self.PAD, pady=self.PAD)
 
     def _create_entry(self):
         entry = ttk.Entry(self.frame, textvariable=self.value)
-        entry.pack()
+        entry.grid()
     
-    def _create_button(self,name):
-        button = ttk.Button(self.frame, text=name, command=lambda button=name: self.controller.onClick(button))
-        button.pack()
+    def _create_label(self):
+        label1 = ttk.Label(self.frame, textvariable=self.value)
+        label1.grid()
+    
+    def _create_button(self,name,row,column):
+        button = ttk.Button(text=name, command=lambda button=name: self.controller.onClick(button))
+        button.grid(row=row, column=column, sticky="nsew")
