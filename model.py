3e9fe096fb221d3df52ff3772e896413ba1405b6
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-29 00:52:41 -0700

    added cases per button

--- model.py
+++ model.py
@@ -1,27 +1,34 @@
 class Model():
     def __init__(self):
         self.value = ''
+        self.operator = ''
 
-    def multiply(a,b):
-        return a * b
-
-
-    def calculate(self, name):
+    def calculate(self, button_name):
         #Check to see if the list is empty.
         #Determine if the button is a number or not.
-        is_int = isinstance(name, int)
-
+        is_int = isinstance(button_name, int)
+        
+        # self.final_val = self.first_val + self.current_val
+    
         #if it IS a number, it will concatenate the current number value to the Entry object
         match is_int:
-            case True : self.value += str(name)
+            case True : self.value += str(button_name)
             case False : pass
 
-        match name:
-            case "C":
+        match button_name:
+            case 'C':
                 self.value = ''
-            case "+":
-                pass
-            case "/":
-                pass
+            case '+'|'-'|'/'|'*':
+                self.operator = button_name
+                self.first_val = self.value
+                self.value += str(button_name)
+            case "=":
+                self.value = str(self._eval())
+
+        if self.value:
+            pass
 
         return self.value
+    
+    def _eval(self):
+        return eval(self.first_val+self.operator+self.value)
