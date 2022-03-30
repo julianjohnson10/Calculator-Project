d131757336a0878640764b1a05503c831d745d9f
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-30 00:10:32 -0700

    semi working operations

--- model.py
+++ model.py
@@ -12,23 +12,31 @@ class Model():
     
         #if it IS a number, it will concatenate the current number value to the Entry object
         match is_int:
-            case True : self.value += str(button_name)
+            case True : 
+                self.value += str(button_name)
+
+
             case False : pass
 
         match button_name:
             case 'C':
                 self.value = ''
-            case '+'|'-'|'/'|'*':
+            case '+'|'-':
                 self.operator = button_name
                 self.first_val = self.value
-                self.value += str(button_name)
+                self.value += button_name
+            case '×':
+                self.operator = '*'
+                self.first_val = self.value
+                self.value += button_name
+            case '÷':
+                self.operator = '/'
+                self.first_val = self.value
+                self.value += button_name
             case "=":
                 self.value = str(self._eval())
-
-        if self.value:
-            pass
 
         return self.value
     
     def _eval(self):
-        return eval(self.first_val+self.operator+self.value)
+        return eval(self.value)
